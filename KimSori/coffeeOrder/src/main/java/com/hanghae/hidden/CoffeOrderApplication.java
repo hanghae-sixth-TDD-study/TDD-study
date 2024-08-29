8a5324a5ecf87e89efe1eecd6b0637846d9afd08
KimSori <noksm2@naver.com>
2024-08-30 00:01:18 +0900

    [ADD] CREATE Project

---
+++ KimSori/coffeeOrder/src/main/java/com/hanghae/hidden/CoffeOrderApplication.java
@@ -0,0 +1,12 @@
+package com.hanghae.hidden;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeOrderApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
